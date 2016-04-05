8eca7748bd0dc60e1716e30b9a6f12a9d309b607
Blue Bells <macroramesh6@gmail.com>
2016-04-05 04:11:43 -0400

    run job with forever

--- scripts/run_app.sh
+++ scripts/run_app.sh
@@ -1,34 +1,17 @@
 set -e
 
+echo "yahoo you are entered"
+ls /jobs
 if [ -d /jobs ]; then
+  echo "yes am in"
   cd /jobs
-  tar xzf *.tar.gz
-  cd /jobs/jobs/
+  tar xzf jobs.tar.gz
+  cd /jobs/jobs
   npm i
 else
-  echo "=> You don't have an meteor app to run in this image."
+  echo "=> You don't have jobs to run in this image."
   exit 1
 fi
 
-#if [[ $REBUILD_NPM_MODULES ]]; then
-#  if [ -f /opt/meteord/rebuild_npm_modules.sh ]; then
-#    cd programs/server
-#    bash /opt/meteord/rebuild_npm_modules.sh
-#    cd ../../
-#  else
-#    echo "=> Use meteorhacks/meteord:bin-build for binary bulding."
-#    exit 1
-#  fi
-#fi
-
-# Set a delay to wait to start meteor container
-if [[ $DELAY ]]; then
-  echo "Delaying startup for $DELAY seconds"
-  sleep $DELAY
-fi
-
-# Honour already existing PORT setup
-# export PORT=${PORT:-80}
-
 echo "=> Starting Job server"
-node server.js
+forever --minUptime 2000 --spinSleepTime 1000 server.js
